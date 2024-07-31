4b9f43a0b165db214340457c0c59e832841175e1
Phillyp Henning <phillyp.henning@gmail.com>
2024-07-30 20:55:04 -0400

    Adding GOAP

---
+++ scripts/combat/GOAP/AI_Agent.gd
@@ -0,0 +1,176 @@
+extends Node
+
+class_name AI_Agent
+
+const GoalPack = preload("res://scripts/combat/GOAP/Goal.gd")
+const ActionPack = preload("res://scripts/combat/GOAP/Action.gd")
+const PlannerPack = preload("res://scripts/combat/GOAP/Planner.gd")
+const UtilsPack = preload("res://scripts/combat/GOAP/Utils.gd")
+const Characteristics = preload("res://scripts/combat/creatures/combat_creature_characteristics.gd")
+var Utils = UtilsPack.new()
+
+enum SEVERITY_LEVEL {
+	NONE = 0,
+	MINOR = 30,
+	MIDDLING = 50,
+	SEVERE = 70,
+	DESPERATE = 90,
+	MAX = 100,
+}
+
+var primary_goals: Array = []
+var current_plan: Array = []
+var static_actions: Array = [
+	ActionPack.new("DoTheAntsyShuffle", 
+		{
+			"antsy": func(a): return a > 0, # Precondition: Antsy should be lower than "0"
+		},
+		{	# Effects
+			"antsy": -1,
+		}, 
+	),
+	ActionPack.new("MoveTowardsTarget", 
+		{
+			"target_in_attack_range": false
+		},
+		{	# Effects
+			"target_in_attack_range": true
+		},
+	),
+	ActionPack.new("BreakLineOfSight", 
+		{	
+			"has_los": true
+		},
+		{	# Effects
+			"has_los": false,
+		},
+	),
+]
+var available_actions: Array = []
+
+var simulated_character: Dictionary = {
+	"health": 10.0,
+	"max_health": 10.0,
+	"health_severity": SEVERITY_LEVEL.NONE,
+	
+	"stamina": 8.0,
+	"max_stamina": 8.0,
+	"stamina_severity": SEVERITY_LEVEL.NONE,
+	
+	"antsy": 0.0,
+	"max_antsy": 5.0,
+	"antsy_severity": SEVERITY_LEVEL.NONE,
+	
+	"has_los": true,
+	
+	"target_in_attack_range": false,
+	"character_can_attack": true,
+	
+	"defeat_enemy": false,
+	
+	"character_is_being_attacked": false,
+	"character_is_defending": false,
+	
+	"speed": 300,
+	"character_position": Vector2(0,0),
+	"target_position": Vector2(300,300),
+}
+
+var game_start_time: float
+
+var goal_keep_moving: Goal = GoalPack.new()
+var goal_conserve_health: Goal = GoalPack.new()
+var goal_attack_enemy: Goal = GoalPack.new()
+var goal_defend_against_attack: Goal = GoalPack.new()
+
+var character_node: CombatCreatureBaseClass
+
+# Debug
+var current_plan_text: TextEdit
+var goals_textbox: TextEdit
+
+
+func _ready(): 
+	# Improvements that could be made:
+	##	1. conserve_health
+	##		- Conserve health goes beyond breaking los though this is a good start. Another option for "Conserving health" would be to focus on defensive actions. In this scenario, when a creature takes damage it would temporarily boost the priority of the "defense_action" goal. 
+	primary_goals.append(goal_keep_moving.new_goal_with_timer("keep_moving", calculate_keep_moving_priority, 1, keep_moving_interval_increase, get_parent(), {"antsy": 0}))
+	primary_goals.append(goal_conserve_health.new_goal_with_timer("conserve_health", calculate_conserve_health_priority, 2.5, conserve_health_interval_decrease, get_parent(), {"has_los": false}))
+	primary_goals.append(goal_attack_enemy.new_goal_with_static_priority("defeat_enemy", 4.5, {"defeat_enemy": true}))
+	primary_goals.append(goal_defend_against_attack.new_goal_with_static_priority("defense_action", 7, {"character_is_defending": true}))
+
+	character_node = get_parent()
+	
+	# Debug
+	var root_node = get_tree().current_scene
+	current_plan_text = root_node.find_child("CurrentPlanText")
+	goals_textbox = root_node.find_child("GoalsText")
+	
+
+func _init():
+	game_start_time = Time.get_unix_time_from_system()
+
+
+func _process(_delta: float) -> void:
+	determine_goal_priority()
+	#run_planner()
+	#check_if_enemy_is_defeated()
+	debug_available_plan()
+	debug_goals()
+
+
+
+
+
+# Takes an array of goals, runs the "calculate_goal_priority" function which either calls the goal callable or returns the static priority. Sorts the goals from highest priority to lowest.
+func determine_goal_priority():
+	var current_goal_priorities: Array = []
+	for i in range(primary_goals.size()):
+		primary_goals[i].calculate_goal_priority(character_node)
+		current_goal_priorities.append(primary_goals[i].goal_priority)
+
+func run_planner() -> void:
+	var planner = PlannerPack.new()
+	current_plan = planner.build_plan(available_actions, static_actions, primary_goals, simulated_character)
+
+
+
+
+
+##-- GOAL CALCULATIONS --##
+func calculate_conserve_health_priority(_character: Object) -> float:
+	return goal_conserve_health.goal_priority
+
+func conserve_health_interval_decrease() -> void:
+	goal_conserve_health.goal_priority -= .5
+
+func calculate_keep_moving_priority(character: Object) -> float:
+	return character.combat_creature_movement_characteristics.antsy
+
+func keep_moving_interval_increase() -> void:
+	character_node.combat_creature_movement_characteristics.antsy = clamp((character_node.combat_creature_movement_characteristics.antsy+.3), 0, character_node.combat_creature_movement_characteristics.max_antsy)
+
+func check_if_enemy_is_defeated() -> void:
+	simulated_character.defeat_enemy = false
+##-- -------------- --##
+
+
+
+
+
+##-- DEBUG --##
+func debug_available_plan() -> void:
+	var text_string: String
+	text_string = "Current Plan:"
+	for action in current_plan:
+		text_string = "{text_string}\n\tAction: [{name}]".format({"text_string": text_string, "name": action.action_name})
+	current_plan_text.text = text_string
+
+func debug_goals() -> void:
+	var goals_text: String
+	for goal in primary_goals:
+		goals_text = "{goal_text}\nGoal: [{goal}]\n\tPriority: [{priority}]\n\tCriteria:".format({"goal_text": goals_text, "goal": goal.goal_name, "priority": goal.goal_priority})
+		for criteria in goal.goal_criteria:
+			goals_text = "{goal_text}\n\t\t[{criteria}] : [{value}]".format({"goal_text": goals_text, "criteria": criteria, "value": goal.goal_criteria[criteria]})
+	goals_textbox.text = goals_text
+##-- ----- --##
---
+++ scripts/combat/GOAP/Planner.gd
@@ -0,0 +1,82 @@
+extends Node
+class_name Planner
+
+# build_plan
+# 	1. Combines available_actions and static_actions
+#	2. Sorts the primary_goals according to their goal_priority
+#	3. Creates an empty plan array
+#	4. Cycles through each goal in primary_goals
+#		a. Attempts to build a plan (an array of actions to take to meet a goal)
+#		b. If the plan is returned empty, then it moves on to the next goal
+#		c. If the plan isn't empty it returns the plan (Array of actions)
+#	5. Return the empty plan
+func build_plan(available_actions: Array, static_actions: Array, primary_goals: Array, world_state: Dictionary) -> Array:
+	# Combine static and available actions
+	var all_actions = available_actions + static_actions
+	
+	# Get the highest priority goal
+	primary_goals.sort_custom(func (a, b): return a.goal_priority > b.goal_priority)
+
+	# Initialize the plan as an empty array
+	var plan = []
+
+	# Use A* algorithm or any other suitable planning algorithm to build the plan
+	for goal in primary_goals:
+		if build_node_plan("a*", plan, all_actions, goal, world_state, goal.goal_criteria):
+			if plan.is_empty():
+				continue
+			else:
+				return plan
+	return plan
+	
+# Recursively build a plan (One of A*, DFS, or BFS)(A* is the only implemented one at this time)
+func build_node_plan(algorithm: String, plan: Array, actions: Array, goal: Goal, current_state: Dictionary, goal_criteria: Dictionary) -> bool:
+	match algorithm:
+		"a*":
+			# Base Case: Check if the current state already satisfies the goal
+			if satisfies_goal(current_state, goal_criteria):
+				return true
+
+			# Find actions that will satisfy the goals desired world state
+			# For example:
+			#	For the goal "Keep moving" the desired world state is to have "antsy" at 0
+			var valid_actions = []
+			for criteria_key in goal_criteria:
+				for action in actions:
+					if action.is_valid(current_state, criteria_key):
+						valid_actions.append(action)
+						# Add any preconditions to the goal_criteria list
+						# Example:
+						##	AttackTarget is used to "defeatenemy"
+						## 	But AttackTarget requires that the character is in range
+						##	The precondition of "GetInRange" is now required to build a plan
+						goal_criteria.merge(action.preconditions)
+			
+			# Improvement: Sorting
+			##	- If multiple actions accomplish the same action, then it will become necessary to filter the actions down to the "best option"
+
+			# Iterate over valid actions and attempt to build a plan
+			for action in valid_actions:
+
+				# Create a hypothetical new state by applying the action
+				var new_state = action.apply(current_state)
+
+				# Add the action to the current plan
+				plan.append(action)
+
+				# Recursively attempt to build the rest of the plan with the new state
+				if build_node_plan("a*", plan, actions, goal, new_state, goal_criteria):
+					return true
+
+				# Backtrack if the current action did not lead to a valid plan
+				plan.pop_back()
+	return false
+
+
+func satisfies_goal(agent_state: Dictionary, goal_criteria: Dictionary) -> bool:
+	var tracker: bool = false
+	for key in goal_criteria.keys():
+		tracker = agent_state.get(key) == goal_criteria[key]
+		if !tracker:
+			return false
+	return tracker
