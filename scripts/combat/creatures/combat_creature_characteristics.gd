d80a3e1aee2ed587f28fb487671a145d1a4bd84f
Phillyp Henning <phillyp.henning@gmail.com>
2024-08-03 18:55:09 -0400

    commenting out health priority (for now)

--- scripts/combat/creatures/combat_creature_characteristics.gd
+++ scripts/combat/creatures/combat_creature_characteristics.gd
@@ -9,15 +9,15 @@ enum CHARACTER_TYPE {
 	NPC_ENEMY
 }
 
-enum SEVERITY_LEVEL {
-	NONE = 0,
-	TRIVIAL = 10,
-	MINOR = 30,
-	MIDDLING = 50,
-	SEVERE = 70,
-	DESPERATE = 90,
-	MAX = 100,
-}
+# enum SEVERITY_LEVEL {
+# 	NONE = 0,
+# 	TRIVIAL = 10,
+# 	MINOR = 30,
+# 	MIDDLING = 50,
+# 	SEVERE = 70,
+# 	DESPERATE = 90,
+# 	MAX = 100,
+# }
 
 enum PROXIMITY {
 	MELEE_CLOSE = 75,
@@ -32,7 +32,7 @@ var character_name: String
 # -- HEALTH -- #
 var current_health: float
 var max_health: float
-var health_severity: SEVERITY_LEVEL
+#var health_severity: SEVERITY_LEVEL
 var is_dead: bool
 var can_take_damage: bool
 var can_take_damage_after_time: float
@@ -64,36 +64,35 @@ var los_on_target: bool
 var current_ideal_range: PROXIMITY
 
 
-func calculate_severity_level(state: Dictionary, key: String, reversed: bool = false) -> Dictionary:
-	var severity_current =  state.get("current_{key}".format({"key": key}), false)
-	var severity_max = state.get("max_{key}".format({"key": key}), false)
-	var severity = state.get("{key}_severity".format({"key": key}), false)
+# func calculate_severity_level(state: Dictionary, key: String, reversed: bool = false) -> Dictionary:
+# 	var severity_current =  state.get("current_{key}".format({"key": key}), false)
+# 	var severity_max = state.get("max_{key}".format({"key": key}), false)
+# 	var severity = state.get("{key}_severity".format({"key": key}), false)
 
-	if severity_current == severity_max:
-		if reversed:
-			severity = SEVERITY_LEVEL.MAX
-		else:
-			severity = SEVERITY_LEVEL.NONE
-	else:
-		var formula: float
-		if reversed:
-			formula = ((severity_current / severity_max) * 100)
-		else:
-			formula = 100 - ((severity_current / severity_max) * 100)
-		if formula == SEVERITY_LEVEL.MAX:
-			severity = SEVERITY_LEVEL.MAX
-		elif formula >= SEVERITY_LEVEL.DESPERATE:
-			severity = SEVERITY_LEVEL.DESPERATE
-		elif formula >= SEVERITY_LEVEL.SEVERE:
-			severity = SEVERITY_LEVEL.SEVERE
-		elif formula >= SEVERITY_LEVEL.MIDDLING:
-			severity = SEVERITY_LEVEL.MIDDLING
-		elif formula >= SEVERITY_LEVEL.MINOR:
-			severity = SEVERITY_LEVEL.MINOR
-		elif formula > SEVERITY_LEVEL.NONE:
-			severity = SEVERITY_LEVEL.TRIVIAL
-		else:
-			severity = SEVERITY_LEVEL.NONE
-	state["{key}_severity".format({"key": key})] = severity
-	return state
-
+# 	if severity_current == severity_max:
+# 		if reversed:
+# 			severity = SEVERITY_LEVEL.MAX
+# 		else:
+# 			severity = SEVERITY_LEVEL.NONE
+# 	else:
+# 		var formula: float
+# 		if reversed:
+# 			formula = ((severity_current / severity_max) * 100)
+# 		else:
+# 			formula = 100 - ((severity_current / severity_max) * 100)
+# 		if formula == SEVERITY_LEVEL.MAX:
+# 			severity = SEVERITY_LEVEL.MAX
+# 		elif formula >= SEVERITY_LEVEL.DESPERATE:
+# 			severity = SEVERITY_LEVEL.DESPERATE
+# 		elif formula >= SEVERITY_LEVEL.SEVERE:
+# 			severity = SEVERITY_LEVEL.SEVERE
+# 		elif formula >= SEVERITY_LEVEL.MIDDLING:
+# 			severity = SEVERITY_LEVEL.MIDDLING
+# 		elif formula >= SEVERITY_LEVEL.MINOR:
+# 			severity = SEVERITY_LEVEL.MINOR
+# 		elif formula > SEVERITY_LEVEL.NONE:
+# 			severity = SEVERITY_LEVEL.TRIVIAL
+# 		else:
+# 			severity = SEVERITY_LEVEL.NONE
+# 	state["{key}_severity".format({"key": key})] = severity
+# 	return state
